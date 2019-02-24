849fd97a2a6f666196d1b4fa1f579fa66a8ecb86
Willa Jin <jin.willa@gmail.com>
2019-02-24 01:40:21 -0800

    added devour button and fixed put method

--- config/orm.js
+++ config/orm.js
@@ -58,7 +58,7 @@ var orm = {
     // update table entry
     updateOne: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
-        queryString += " SET";
+        queryString += " SET ";
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
         queryString += condition;
